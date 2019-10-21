318d7064049d372115dde13ef9af2912dc68eaff
Chris Mallery <chmaller@microsoft.com>
2019-10-21 08:46:35 -0700

    Started packet filter for PS3

--- src/Plugins/Filters/PacketFilter_PS3.cpp
+++ src/Plugins/Filters/PacketFilter_PS3.cpp
@@ -10,24 +10,41 @@ PacketFilter_PS3::PacketFilter_PS3(ConfigParser *cfg) {
 }
 
 void PacketFilter_PS3::filter_packet(Packet* packet) {
-	if (packet->wLength<=64) {
-		char* hex=hex_string((void*)packet->data,packet->wLength);
-		fprintf(file,"%02x[%d]: %s\n",packet->bEndpoint,packet->wLength,hex);
-		free(hex);
+	if (packet->wLength == 49) {
+		
+		// digital buttons
+		for (int j = 5; j < 8; ++j)
+			for (int i = 0; i < 8; ++i)
+				fprintf(file, "%d", (packet->data[j] & (1 << i)) != 0);
+
+		// sticks
+		for (int j = 1; j < 5; ++j)
+			for (int i = 0; i < 8; ++i)
+				fprintf(file, "%d", (packet->data[j] & (1 << i)) != 0);
+
+		// triggers
+		for (int j = 8; j < 5; ++j)
+			for (int i = 0; i < 8; ++i)
+				fprintf(file, "%d", (packet->data[j] & (1 << i)) != 0);
+
+		// analog buttons
+		for (int j = 9; j < 19; ++j)
+			for (int i = 0; i < 8; ++i)
+				fprintf(file, "%d", (packet->data[j] & (1 << i)) != 0);
 	}
 }
 void PacketFilter_PS3::filter_setup_packet(SetupPacket* packet,bool direction) {
-	if (packet->ctrl_req.wLength && packet->data) {
-		char* hex_setup=hex_string(&(packet->ctrl_req),sizeof(packet->ctrl_req));
-		char* hex_data=hex_string((void*)(packet->data),packet->ctrl_req.wLength);
-		fprintf(file,"[%s]: %s\n",hex_setup,hex_data);
-		free(hex_data);
-		free(hex_setup);
-	} else {
-		char* hex_setup=hex_string(&(packet->ctrl_req),sizeof(packet->ctrl_req));
-		fprintf(file,"[%s]\n",hex_setup);
-		free(hex_setup);
-	}
+	//if (packet->ctrl_req.wLength && packet->data) {
+	//	char* hex_setup=hex_string(&(packet->ctrl_req),sizeof(packet->ctrl_req));
+	//	char* hex_data=hex_string((void*)(packet->data),packet->ctrl_req.wLength);
+	//	fprintf(file,"[%s]: %s\n",hex_setup,hex_data);
+	//	free(hex_data);
+	//	free(hex_setup);
+	//} else {
+	//	char* hex_setup=hex_string(&(packet->ctrl_req),sizeof(packet->ctrl_req));
+	//	fprintf(file,"[%s]\n",hex_setup);
+	//	free(hex_setup);
+	//}
 }
 
 static PacketFilter_PS3 *proxy;
