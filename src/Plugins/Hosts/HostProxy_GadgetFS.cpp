f7139d95443b71bbae32464eda441268e76ed14d
Zoggins <zoggins@gmail.com>
2019-11-02 12:47:21 -0700

    fixing mixed up printf arguments

--- src/Plugins/Hosts/HostProxy_GadgetFS.cpp
+++ src/Plugins/Hosts/HostProxy_GadgetFS.cpp
@@ -124,7 +124,7 @@ int HostProxy_GadgetFS::generate_descriptor(Device* device) {
 
 				    // conversion is: newValue = log2(8*oldValue)+1
 				    int newValue = (log10(8*(epd->bInterval))/log10(2)) + 1;
-					fprintf(stderr, "[%02x]: old bInterval: %02X\n[%02x]: calculated new bInterval: %02X\n", epd->bEndpointAddress, epd->bInterval, newValue, epd->bEndpointAddress);
+					fprintf(stderr, "[%02x]: old bInterval: %02X\n[%02x]: calculated new bInterval: %02X\n", epd->bEndpointAddress, epd->bInterval, epd->bEndpointAddress, newValue);
 				    memset(&epd->bInterval,newValue,1);
 				    pointer+= epd->bLength;
 				}
