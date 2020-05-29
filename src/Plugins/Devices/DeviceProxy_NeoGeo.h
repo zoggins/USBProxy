4ec8cff6ccb06e3f0aa31596d6cfd800d38a9baa
Zoggins <zoggins@gmail.com>
2020-05-29 09:50:06 -0700

    more typos

--- src/Plugins/Devices/DeviceProxy_NeoGeo.h
+++ src/Plugins/Devices/DeviceProxy_NeoGeo.h
@@ -8,19 +8,12 @@
 #include "DeviceProxy_LibUSB.h"
 
 class DeviceProxy_NeoGeo: public DeviceProxy_LibUSB {
-private:
-
-	const char* manufacturer;
-	const char* product;
-	const char* serialNumber;
-
-	bool isSN30Pro();
 
 public:
 	DeviceProxy_NeoGeo(int vendorId = LIBUSB_HOTPLUG_MATCH_ANY, int productId = LIBUSB_HOTPLUG_MATCH_ANY,
 			bool includeHubs = false);
 	DeviceProxy_NeoGeo(ConfigParser *cfg);
-	virtual ~DeviceProxy_Xbox360();
+	virtual ~DeviceProxy_NeoGeo();
 
 protected:
 	virtual bool swallow_setup_packet_ignore_error(const usb_ctrlrequest* setup_packet);
