0f45ed916790abaa15bd73a926f2d1b10b8ae750
Chris Mallery <zoggins@retro-spy.com>
2022-05-22 20:57:09 -0700

    tst

--- src/Plugins/Hosts/HostProxy_GadgetFS.h
+++ src/Plugins/Hosts/HostProxy_GadgetFS.h
@@ -42,7 +42,7 @@ protected:
 	virtual bool init_lock();
 	virtual void destroy_lock();
 	virtual struct aiocb* get_aiocp(int number);
-	virtual (send_completion_handler*)(sigval_t sigval) ret_free_aio_callback_function();
+	virtual (*send_completion_handler)(sigval_t sigval) ret_free_aio_callback_function();
 
 	virtual void handle_USB_REQ_SET_CONFIGURATION();
 	virtual bool do_not_send(__u8 endpoint, int* length);
